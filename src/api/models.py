bbc0e90437f3be099542b5c4c74e8ad3b3831eee
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-06-14 20:07:23 +0000

    Creacion de tabla profesor

--- src/api/models.py
+++ src/api/models.py
@@ -2,10 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+class Professor(db.Model):
+    professorID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    lastname = db.Column(db.String(50), unique=False, nullable=False)
+    photo = db.Column(db.String(50), unique=False, nullable=False)
+    cardIDtype = db.Column(db.String(50), unique=False, nullable=False)
+    numbercardID = db.Column(db.Integer, unique=True, nullable=False)
+    birthday = db.Column(db.Date, unique=False, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    phonenumber = db.Column(db.Integer, unique=True, nullable=False)
+    address = db.Column(db.String(250), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    paymentID = db.Column(db.Integer)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
