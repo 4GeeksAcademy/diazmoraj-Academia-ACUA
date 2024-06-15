685128c5d3df8b595a22d19e1ce825c62386bbcb
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-06-15 03:00:51 +0000

    Creación variable de entorno BACKEND_URL

--- src/front/js/pages/HomeStudent.js
+++ src/front/js/pages/HomeStudent.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 
-export const Demo = () => {
+export const HomeStudent = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
