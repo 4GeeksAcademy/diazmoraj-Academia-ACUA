5da96bfd0766f221f002c0f57243d17b213ec00b
Adrian <142854436+withyso@users.noreply.github.com>
2024-06-20 05:57:03 +0000

    forget to add srcs to previous commit

--- src/front/js/component/NavbarACUA.js
+++ src/front/js/component/NavbarACUA.js
@@ -1,19 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LogButton from "./LogButton";
+import { useState, useEffect } from "react";
+
 
 export const NavbarACUA = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav className="navbar navbar-expand-lg bg-white">
+			<div className="container-fluid">
+				<a className="navbar-brand ps-5" href="#">
+					<img src="https://i.imgur.com/fNjEl6I.png" alt="Bootstrap" className="logo" />
+				</a>
+				<div className="button me-4">
+					<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onChange={(e) => { console.log(e) }}>
+						<i className="fa-solid fa-bars" style={{ fontSize: '35px' }}></i>
+					</button>
+				</div>
+				<div className="collapse navbar-collapse justify-content-center" id="navbarNav" style={{
+					gap: '300px',
+					paddingRight: '45px'
+				}}>
+					<ul className="navbar-nav fw-semibold fs-6 flex-wrap-reverse justify-content-center mt-3">
+						<li className="nav-item">
+							<p className="nav-link" aria-current="page" href="#">Inicio</p>
+						</li>
+						<li className="nav-item">
+							<p className="nav-link" href="#">Planes</p>
+						</li>
+						<li className="nav-item">
+							<p className="nav-link" href="#">Sobre Nosotros</p>
+						</li>
+						<li className="nav-item">
+							<p className="nav-link" href="#">Contacto</p>
+						</li>
+						<li className="nav-item showButton"> <LogButton text="Login" /> </li>
+					</ul>
+				</div>
+				<div className="loginButton pe-5 hiddeButton">
+					<LogButton text="Login" />
 				</div>
 			</div>
-		</nav>
+		</nav >
 	);
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -11,7 +11,7 @@ import { NavbarStudent } from "./pages/NavbarStudent";
 import { LogIn } from "./pages/LogIn"
 import { SignUp } from "./pages/SignUp";
 
-import { HomeACUA } from "./pages/HomeACUA";
+import  HomeACUA  from "./pages/HomeACUA";
 import { HomeAdmin } from "./pages/HomeAdmin";
 import { HomeProfessor } from "./pages/HomeProfessor";
 import { HomeStudent } from "./pages/HomeStudent";
@@ -43,11 +43,14 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <NavbarStudent />
+                    {
+                    //<NavbarStudent />
+                    }
                     <Routes>
 
                         <Route path="/" element={<HomeStudent />} />
                         <Route path="*" element={<h1>Not found!</h1>} />
+                        <Route path="/homeacua" element={<HomeACUA />} />
 
                         {/* <Route element={<NavbarACUA />} path="/NavbarACUA"/>
                         <Route element={<NavbarAdmin />} path="/NavbarAdmin"/>
