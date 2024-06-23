675a2470b75ca4ccbc481f3f91fffa8badfeaa83
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-06-23 21:01:12 +0000

    Se agregó state.actions.getStudents() al UseEffect

--- src/front/js/store/appContext.js
+++ src/front/js/store/appContext.js
@@ -31,6 +31,7 @@ const injectContext = PassedComponent => {
 			state.actions.getMessage(); // <---- calling this function from the flux.js actions
 			state.actions.getCourses();
 			state.actions.getProfessors();
+			state.actions.getStudents();
 		}, []);
 
 		// The initial value for the context is not null anymore, but the current state of this component,
